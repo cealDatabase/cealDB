aa0393335f59f3f23bfb3721ef52def8869024c2
Meng-V <qum@miamioh.edu>
2024-03-17 12:21:01 -0400

    init test. Has bug

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,13 +2,23 @@ import prisma from '../lib/prisma'
 
 async function main() {
   const response = await Promise.all([
+    prisma.library.upsert(
+      {
+        where: { contactPerson: { name: "Lee Robinson" } },
+        update: {},
+        create: {
+          name: "Miami",
+        },
+      }),
     prisma.user.upsert({
       where: { email: 'rauchg@vercel.com' },
       update: {},
       create: {
         name: 'Guillermo Rauch',
         email: 'rauchg@vercel.com',
-        library: "UCSD",
+        library: {
+          name: "Miami"
+        },
         image:
           'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
       },
@@ -19,7 +29,9 @@ async function main() {
       create: {
         name: 'Lee Robinson',
         email: 'lee@vercel.com',
-        library: "UCLA",
+        library: {
+          name: "Miami"
+        },
         image:
           'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
       },
@@ -30,7 +42,9 @@ async function main() {
       create: {
         name: 'Steven Tey',
         email: 'stey@vercel.com',
-        library: "UW-Madison",
+        library: {
+          name: "Miami"
+        },
         image:
           'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
       },
