a09a7a39f231515adfc40875c8ed437841564afd
Meng-V <qum@miamioh.edu>
2024-03-19 22:43:53 -0400

    still don't work

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,63 +1,60 @@
-import prisma from '../lib/prisma'
+import prisma from "../lib/prisma";
 
 async function main() {
   const response = await Promise.all([
-    prisma.library.upsert(
-      {
-        where: { contactPerson: { name: "Lee Robinson" } },
-        update: {},
-        create: {
-          name: "Miami",
-        },
-      }),
-    prisma.user.upsert({
-      where: { email: 'rauchg@vercel.com' },
-      update: {},
-      create: {
-        name: 'Guillermo Rauch',
-        email: 'rauchg@vercel.com',
-        library: {
-          name: "Miami"
-        },
-        image:
-          'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
-      },
+    await prisma.library.createMany({
+      data: [
+        { name: "UCB" }, 
+        { name: "UCLA" }, 
+        { name: "UW-Madison" }
+      ],
+      skipDuplicates: true,
     }),
-    prisma.user.upsert({
-      where: { email: 'lee@vercel.com' },
-      update: {},
-      create: {
-        name: 'Lee Robinson',
-        email: 'lee@vercel.com',
+    await prisma.user.createMany({
+      data: [
+        { 
+        name: "Guillermo Rauch", 
+        email: "rauchg@vercel.com", 
+        image: "https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg",
         library: {
-          name: "Miami"
+          connectOrCreate:{
+            where: {name: "UCLA"},
+            create: {name: "UCLA"},
+          },
         },
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
-      },
-    }),
-    await prisma.user.upsert({
-      where: { email: 'stey@vercel.com' },
-      update: {},
-      create: {
-        name: 'Steven Tey',
-        email: 'stey@vercel.com',
+      }, 
+      { 
+        name: "Lee Robinson", 
+        email: "lee@vercel.com", 
+        image: "https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg",
         library: {
-          name: "Miami"
+          connectOrCreate:{
+            where: {name: "UW-Madison"},
+            create: {name: "UW-Madison"},
+          },
         },
-        image:
-          'https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg',
-      },
-    }),
-  ])
-  console.log(response)
+      }, 
+      { 
+        name: "Steven Tey", 
+        email: "stey@vercel.com", 
+        image: "https://images.ctfassets.net/e5382hct74si/4QEuVLNyZUg5X6X4cW4pVH/eb7cd219e21b29ae976277871cd5ca4b/profile.jpg",
+        library: {
+          connectOrCreate:{
+            where: {name: "UCB"},
+            create: {name: "UCB"},
+          },
+        },
+      }, 
+      ]})
+  ]);
+  console.log(response);
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
