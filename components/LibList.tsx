f3cd8ae5a1379a2f3be1f63d8a63281cfc83eaf3
Meng-V <qum@miamioh.edu>
2024-04-21 22:43:58 -0400

    Update LibList.tsx

--- components/LibList.tsx
+++ components/LibList.tsx
@@ -7,6 +7,8 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { SingleLibraryType } from "@/types/types"; // Import the LibraryType type
+import Link from "next/link";
+import { Button, Stack } from "@mui/material";
 
 export default function LibGrid({
   libraries,
@@ -20,22 +22,33 @@ export default function LibGrid({
   };
 
   return (
-    <Box sx={{ minWidth: 240 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Select an Institution</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={institution}
-          label="Select an institution"
-          onChange={handleChange}
-        >
-          {Array.isArray(libraries) &&
-            libraries.map((library: SingleLibraryType) => (
-              <MenuItem value={library.id} key={library.id}>{library.name}</MenuItem>
-            ))}
-        </Select>
-      </FormControl>
-    </Box>
+    <Stack direction="row" spacing={2} alignItems="center">
+      <Box sx={{ minWidth: 240 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">
+            Select an Institution
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={institution}
+            label="Select an institution"
+            onChange={handleChange}
+          >
+            {Array.isArray(libraries) &&
+              libraries.map((library: SingleLibraryType) => (
+                <MenuItem value={library.id} key={library.id}>
+                  {library.name}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+      </Box>
+      <Link href={`/libraries/${institution}`}>
+        <Button variant="contained" size="large" className="py-3 bg-orange-500 hover:bg-red-500">
+          Find
+        </Button>
+      </Link>
+    </Stack>
   );
 }
