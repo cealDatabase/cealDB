0d6aa2b6001c7950477ba68b0fe9da578fc68d73
Meng-V <qum@miamioh.edu>
2024-04-23 17:54:39 -0400

    added email protection

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,14 +1,37 @@
+"use client"
+
+import { Container } from "@/components/Container";
+import Button from '@mui/material/Button';
 import Link from "next/link";
 
 const HelpPage = () => {
+  function copyEmail(){
+    return ("mailto:anlin.yang@wisc.edu")
+  }
   return (
     <main>
       <h1>Help Page</h1>
+      <Container className="flex flex-col space-y-4">
+        <h2>How to Cite</h2>
+        <p>
+          Council on East Asian Libraries. Council on East Asian Libraries
+          Statistics. in University of Kansas Libraries [database online].
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lawrence,
+          KS, ©2024. Available from{" "}
+          <Link href="/">https://ceal-db.vercel.app/</Link>
+        </p>
+        <h2>New Libraries</h2>
+        <p>
+          New library/institution need to establish an account to join the
+          database. Contact <Button onClick={copyEmail} variant="outlined" size="small">Anlin Yang</Button> to complete a new
+          Library Information Form. The Library Information Form is used as an
+          application to join the CEAL Statistics annual survey.
+        </p>
+        <h2>Change Password</h2>
 
-      <h2>How to Cite</h2>
-      <h2>New Libraries</h2>
-      <h2>Change Password</h2>
-      <h2>CEAL Statistics Online Forms</h2>
+        <h2>CEAL Statistics Online Forms</h2>
+      </Container>
     </main>
   );
 };
--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,6 +1,6 @@
-import { getAllUsers } from '@/data/fetchPrisma'
-import { timeAgo } from '@/lib/utils'
-import RefreshButton from './refresh-button'
+import { getAllUsers } from "@/data/fetchPrisma";
+import { timeAgo } from "@/lib/utils";
+import RefreshButton from "./refresh-button";
 
 export default async function UserList() {
   const users = await getAllUsers();
@@ -9,32 +9,34 @@ export default async function UserList() {
     <div className="bg-white/70 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
         <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
+          <h2>Recent Users</h2>
         </div>
-        <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
-        {users && users.map((user) => (
-          <div
-            key={user.id}
-            className="flex items-center justify-between py-3"
-          >
-            <div className="flex items-center space-x-4">
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.id}</p>
-                <p className="font-medium leading-none">{user.firstName}</p>
-                <p className="font-medium leading-none">{user.lastName}</p>
-                <p className="font-medium leading-none">{user.email}</p>
-                <p className="font-medium leading-none">{user.positionTitle}</p>
-                <p className="text-sm text-gray-500">{user.libraryId}</p>
-                <p className="text-sm text-gray-500">{user.password}</p>
-                <p className="text-sm text-gray-500">{user.role}</p>
+        {users &&
+          users.map((user) => (
+            <div
+              key={user.id}
+              className="flex items-center justify-between py-3"
+            >
+              <div className="flex items-center space-x-4">
+                <div className="space-y-1">
+                  <p className="font-medium leading-none">{user.id}</p>
+                  <p className="font-medium leading-none">{user.firstName}</p>
+                  <p className="font-medium leading-none">{user.lastName}</p>
+                  <p className="font-medium leading-none">{user.email}</p>
+                  <p className="font-medium leading-none">
+                    {user.positionTitle}
+                  </p>
+                  <p className="text-sm text-gray-500">{user.libraryId}</p>
+                  <p className="text-sm text-gray-500">{user.password}</p>
+                  <p className="text-sm text-gray-500">{user.role}</p>
+                </div>
               </div>
+              <p className="text-sm text-gray-500">{timeAgo(user.updatedAt)}</p>
             </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.updatedAt)}</p>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
-  )
+  );
 }
