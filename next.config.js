85e14585949a58390ed1940e6d45bf8ace597747
Meng-V <qum@miamioh.edu>
2024-05-07 12:09:36 -0400

    fix error

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
     domains: ["images.ctfassets.net"],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
