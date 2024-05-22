d2c987cfef5906a3dc8811785f1d629055f47bf4
Meng-V <qum@miamioh.edu>
2024-05-22 12:09:08 -0400

    sign up works

---
+++ app/api/signup/route.ts
@@ -0,0 +1,36 @@
+import validateEmail from "@/lib/validateEM";
+import validatePassword from "@/lib/validatePW";
+import bcrypt from "bcryptjs";
+import db from "@/lib/db";
+
+export async function POST(request: Request) {
+  // read data off request body
+  const body = await request.json();
+  const { email, password } = body;
+
+  // validate data
+  if (!email || !password) {
+    return Response.json(
+      {
+        error: "Invalid email or password",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  // hash the password
+  // const hash = bcrypt.hashSync(password, 8);
+
+  // create a user in db. Will move to /data/fetchPrisma.ts in the future
+  await db.user.create({
+    data: {
+      email: "vivoequeen@gmail.com",
+      password: "123456",
+    },
+  });
+
+  // return something
+  return Response.json({});
+}
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,34 @@
 import { Container } from "@/components/Container";
+import { redirect } from "next/navigation";
 
 const SignUpPage = () => {
+  
+  async function signup(formData: FormData) {
+    "use server";
+    // Get data off form
+    const email = formData.get("email");
+    const password = formData.get("password");
+    // Send to our api route
+
+    const res = await fetch(process.env.ROOT_URL + "/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = await res.json();
+    console.log(json);
+    // Redirect to log in if success
+    if (res.ok) {
+      redirect("/signin");
+    }
+  }
   return (
     <Container>
       <h1>Sign Up Page</h1>
-      <div>SignUpPage</div>
-      <form>
+      <div className="text-center">SignUpPage</div>
+      <form action={signup}>
         <input type="email" name="email" />
         <input type="password" name="password" />
         <button type="submit">Sign Up</button>
