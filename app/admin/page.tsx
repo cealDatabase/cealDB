e71c9ebd63dc86df005d335f11955b527be49aed
Meng-V <qum@miamioh.edu>
2024-05-22 10:36:30 -0400

    created admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/Container";
+
+const AdminPage = () => {
+  return (
+    <Container>
+      <h1>Hello Admin</h1>
+      <div className="text-center">Admin Page</div>
+    </Container>
+  );
+};
+
+export default AdminPage;
