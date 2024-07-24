f6e125c2da066866b7252f7d22b64161897c629b
Meng-V <qum@miamioh.edu>
2024-07-24 02:07:22 -0400

    time diff done

--- app/(authentication)/forgot/[username]/page.tsx
+++ app/(authentication)/forgot/[username]/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { getUserByUserName } from "../../../../data/fetchPrisma";
-import TimeDiff from "./time";
 
 export const dynamic = "force-dynamic";
 
@@ -17,30 +16,71 @@ function normalizeUsername(rawUsername: string): string {
 
 export default async function SingleLibraryInfoHomePage({
   params,
+  searchParams,
 }: {
   params: { username: string };
+  searchParams: { token: number };
 }) {
   const userItem = await UserSinglePage(normalizeUsername(params.username));
+  const isValid =
+    Math.abs(Number(Date.now()) - searchParams.token) <= 15 * 60 * 1000 // 15 minutes
+      ? true
+      : false;
+
+  console.log(isValid);
 
   return (
     <main>
-      <TimeDiff />
-      {userItem && (
-        <>
-          <div key={userItem.id}>
-            <div>{userItem.id}</div>
-            <div>{userItem.username}</div>
-            <div>{userItem.firstname}</div>
-            <div>{userItem.lastname}</div>
-            <div>{userItem.password}</div>
+      {userItem && !isValid && (
+        <div>
+          <p className="text-3xl font-medium text-gray-900">
+            User found.
+            <br />
+            But validation email <b>expired</b>...
+          </p>
+          <div className="mt-10 flex items-center gap-x-6">
+            <Link
+              href="/forgot"
+              className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+            >
+              Resend Email
+            </Link>
+            <Link
+              href="/"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Back Home <span aria-hidden="true">→</span>
+            </Link>
           </div>
-        </>
+        </div>
+      )}
+      {userItem && isValid && (
+        <div key={userItem.id}>
+          <div>{userItem.id}</div>
+          <div>{userItem.username}</div>
+          <div>{userItem.firstname}</div>
+          <div>{userItem.lastname}</div>
+          <div>{userItem.password}</div>
+        </div>
       )}
       {!userItem && (
         <div>
-          <h2 className="text-red-600">User Not Found</h2>
-          <div className="mt-8">
-            <Link href="/">Back to Homepage</Link>
+          <p className="text-4xl font-medium text-gray-900 text-center">
+            User Not Found
+          </p>
+          <div className="mt-10 flex items-center gap-x-6">
+            <Link
+              href="/forgot"
+              className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+            >
+              Resend Validation Email
+            </Link>
+            <Link
+              href="/"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Back Home <span aria-hidden="true">→</span>
+            </Link>
           </div>
         </div>
       )}
