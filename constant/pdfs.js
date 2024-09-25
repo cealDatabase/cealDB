2c570a97ecf7a1acf8d7015bb6c2c683a6079595
Meng-V <qum@miamioh.edu>
2024-09-25 11:55:17 -0400

    Update pdfs.js

--- constant/pdfs.js
+++ constant/pdfs.js
@@ -31,14 +31,14 @@ export const pdfs = [
       {
         link: "https://scholarsarchive.byu.edu/jeal/vol2023/iss176/4",
         title:
-          "Council on East Asian Libraries Statistics 2021-2022: For North American Institutions",
+          "Council on East Asian Libraries Statistics 2021-2022: For North American Institutions.",
         journal: "Journal of East Asian Libraries, ",
         appendix: "2023: no. 176, Article 4.",
       },
       {
         link: "https://scholarsarchive.byu.edu/jeal/vol2023/iss176/5",
         title:
-          "Lists of Selected Full-text Databases by Subscription in East Asian Studies",
+          "Lists of Selected Full-text Databases by Subscription in East Asian Studies.",
         journal: "Journal of East Asian Libraries, ",
         appendix: "2023 : no. 176, Article 5.",
       },
@@ -75,8 +75,9 @@ export const pdfs = [
       },
       {
         link: "https://scholarsarchive.byu.edu/jeal/vol2021/iss172/6",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        title:
+          "Lists of Selected Full-text Databases by Subscription in East Asian Studies.",
+        journal: "Journal of East Asian Libraries, ",
         appendix: "2021: No. 172 , Article 6.",
       },
     ],
@@ -85,16 +86,24 @@ export const pdfs = [
     yearValue: "2018-2019",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://scholarsarchive.byu.edu/jeal/vol2020/iss170/4/",
+        title:
+          "Council on East Asian Libraries Statistics 2018-2019: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2020: no. 170, Article 4.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://scholarsarchive.byu.edu/jeal/vol2020/iss170/5/",
+        title:
+          "Lists of Selected Full-text Databases by Subscription in East Asian Studies.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2020 : No. 170 , Article 5.",
+      },
+      {
+        link: "https://scholarsarchive.byu.edu/jeal/vol2020/iss170/12/",
+        title: "2019 CEAL Statistics Summary and Data Analysis.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2020 : No. 170 , Article 12.",
       },
     ],
   },
@@ -102,16 +111,25 @@ export const pdfs = [
     yearValue: "2017-2018",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://scholarsarchive.byu.edu/jeal/vol2019/iss168/5/",
+        title:
+          "Council on East Asian Libraries Statistics 2017-2018: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2019: no. 168, Article 5.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://scholarsarchive.byu.edu/jeal/vol2018/iss166/17",
+        title:
+          "Lists of Selected Full-text Databases by Subscription in East Asian Studies.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2018 : No. 166 , Article 17.",
+      },
+      {
+        link: "https://scholarsarchive.byu.edu/jeal/vol2018/iss166/19",
+        title:
+          "Participating Libraries Organizational Structure and Operation Status, 2017.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2018 : No. 166 , Article 19.",
       },
     ],
   },
@@ -119,16 +137,11 @@ export const pdfs = [
     yearValue: "2016-2017",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
-      },
-      {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://scholarsarchive.byu.edu/jeal/vol2018/iss166/16",
+        title:
+          "Council on East Asian Libraries Statistics 2016-2017: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2018: no. 166, Article 16.",
       },
     ],
   },
@@ -136,16 +149,11 @@ export const pdfs = [
     yearValue: "2015-2016",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
-      },
-      {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://hdl.handle.net/1808/22713",
+        title:
+          "Council on East Asian Libraries Statistics 2015-2016: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "2017: no. 164, Article 3.",
       },
     ],
   },
@@ -153,16 +161,11 @@ export const pdfs = [
     yearValue: "2014-2015",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
-      },
-      {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://hdl.handle.net/1808/20316",
+        title:
+          "Council on East Asian Libraries Statistics 2014-2015: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 162 (February 2016): 41-62.",
       },
     ],
   },
@@ -170,16 +173,18 @@ export const pdfs = [
     yearValue: "2013-2014",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://kuscholarworks.ku.edu/handle/1808/17117",
+        title:
+          "Council on East Asian Libraries Statistics 2013-2014: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 160 (February 2015): 27-46.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://ceal.ku.edu/download/44",
+        title: "Instructions for Completing Online Survey Forms.",
+        journal: "",
+        appendix:
+          "Online Forms are available from October 1 through Dec. 12, 2014 (11:59 pm Central Time).",
       },
     ],
   },
@@ -187,16 +192,11 @@ export const pdfs = [
     yearValue: "2012-2013",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
-      },
-      {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://hdl.handle.net/1808/13414",
+        title:
+          "Council on East Asian Libraries Statistics 2012-2013: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 158 (February 2014): 44-67.",
       },
     ],
   },
@@ -204,16 +204,18 @@ export const pdfs = [
     yearValue: "2011-2012",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://hdl.handle.net/1808/10956",
+        title:
+          "Council on East Asian Libraries Statistics 2011-2012: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 156 (February 2013): 16-38.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://ceal.ku.edu/download/43",
+        title: "Instructions for Completing Online Survey Forms",
+        journal: "",
+        appendix:
+          "Online Forms are available from October 3 through Dec. 7, 2012 (11:59 pm Central Time).",
       },
     ],
   },
@@ -221,16 +223,18 @@ export const pdfs = [
     yearValue: "2010-2011",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10118/1/cealstats2010_11%5B1%5D.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2010-2011: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 154 (February 2012): 53-69.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://ceal.ku.edu/download/42",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
+        appendix:
+          "Online Forms are available from September 24 through Dec. 9, 2011 (11:59 pm Central Time).",
       },
     ],
   },
@@ -238,15 +242,16 @@ export const pdfs = [
     yearValue: "2009-2010",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10120/1/cealstats2009_10.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2009-2010: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 152 (February 2011): 42-57.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/41",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -255,15 +260,16 @@ export const pdfs = [
     yearValue: "2008-2009",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10152/1/cealstats2008_09.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2008-2009: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no.150 (February 2010): 24-43.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/38",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -272,15 +278,16 @@ export const pdfs = [
     yearValue: "2007-2008",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10153/1/cealstat2007_08.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2007-2008: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 147 (February 2009): 50-78.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/40",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -289,15 +296,16 @@ export const pdfs = [
     yearValue: "2006-2007",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10154/1/cealstat06_07.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2006-2007: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 144 (February 2008): 42-69.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/39",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -306,15 +314,16 @@ export const pdfs = [
     yearValue: "2005-2006",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10155/1/cealstat05_06.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2005-2006: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 141 (February 2007): 51-78.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/37",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -323,15 +332,23 @@ export const pdfs = [
     yearValue: "2004-2005",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10157/1/cealstat04_05.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2004-2005: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 138 (February 2006): 59-85.",
+      },
+      {
+        link: "https://ceal.ku.edu/download/36",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "http://1.49.209.21/cealemail2005.pdf",
+        title:
+          "Call for CEAL Statistics 2004/2005 Data Input (E-mail sent to the Eastlib-L, Oct. 10, 2005)",
+        journal: "",
         appendix: "",
       },
     ],
@@ -340,15 +357,16 @@ export const pdfs = [
     yearValue: "2003-2004",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10156/1/cealstat03_04.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2003-2004: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 135 (February 2005): 35-62.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/35",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -357,15 +375,16 @@ export const pdfs = [
     yearValue: "2002-2003",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10158/3/CEALstats_2002_2003.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2002-2003: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 132 (February 2004): 11-35.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/34",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -374,15 +393,29 @@ export const pdfs = [
     yearValue: "2001-2002",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://kuscholarworks.ku.edu/bitstream/handle/1808/11201/Summary%20of%20CEAL%20Statistics%202001-2002.pdf",
+        title: "Council on East Asian Libraries Statistics, 2001-2002.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 129 (Feb, 2003): 77-101.",
+      },
+      {
+        link: "https://ceal.ku.edu/download/33",
+        title: "Instructions for Completing Online Forms",
+        journal: "(updated Dec. 4, 2002)",
         appendix: "",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/32",
+        title:
+          "Call for CEAL Statistics 2001-2002 Survey Input (posted on the Eastlib 10/16/2002)",
+        journal: "",
+        appendix: "",
+      },
+      {
+        link: "https://ceal.ku.edu/download/31",
+        title:
+          "Questions and Answers on the CEAL Statistics 2001 Online Input Form (PDF)",
+        journal: "",
         appendix: "",
       },
     ],
@@ -391,15 +424,16 @@ export const pdfs = [
     yearValue: "2000-2001",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10160/3/cealstats_2000_2001.pdf",
+        title:
+          "Council on East Asian Libraries Statistics 2000-2001: For North American Institutions.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 126 (Feb, 2002): 25-52.",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/30",
+        title: "Instructions for Completing Online Forms",
+        journal: "",
         appendix: "",
       },
     ],
@@ -408,16 +442,24 @@ export const pdfs = [
     yearValue: "1999-2000",
     linkValue: [
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
+        link: "https://ceal.ku.edu/download/45",
+        title:
+          "Council on East Asian Libraries Statistics for North American Institutions, 1999-2000.",
+        journal: "Journal of East Asian Libraries, ",
+        appendix: "no. 123 (Feb, 2001): 13-39.",
+      },
+      {
+        link: "https://ceal.ku.edu/download/29",
+        title: "Instructions for Completing the Questionnaire",
+        journal: "",
         appendix: "",
       },
       {
-        link: "",
-        title: "",
-        journal: "Journal of East Asian Libraries",
-        appendix: "",
+        link: "https://ceal.ku.edu/download/28",
+        title: "Libraries in the ARL Data Files, 1992-98",
+        journal: "",
+        appendix:
+          "http://www.lib.virginia.edu/socsci/arl/1998/institution98.shtml",
       },
     ],
   },
@@ -428,7 +470,7 @@ export const pdfs = [
         link: "http://kuscholarworks.ku.edu/dspace/bitstream/1808/10162/1/CEALstats_1998_1999_revised.pdf",
         title:
           "Council on East Asian Libraries Statistics for North American Institutions, 1998-1999, Revised.",
-        journal: "Journal of East Asian Libraries",
+        journal: "Journal of East Asian Libraries, ",
         appendix: "no. 122 (October, 2000): 105-121.",
       },
       {
