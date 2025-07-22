d1ba3592e17199a09b16e767468ce9cbf993bb29
Meng <43998638+Meng-V@users.noreply.github.com>
2025-07-22 16:40:58 -0400

    added pages

---
+++ app/(authentication)/admin/forms/unprocessed/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <h1>Unprocessed BackLog Materials (volumes or pieces)</h1>
+        </div>
+    )
+}
+
+export default page
